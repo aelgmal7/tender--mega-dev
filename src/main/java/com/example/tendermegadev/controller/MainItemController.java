436761c49cfb6fe3b5db11597382d28357e74c39
aelgmal7 <aelgmal7@gmail.com>
2021-09-13 16:58:44 +0200

    controller and service added

--- src/main/java/com/example/tendermegadev/controller/MainItemController.java
+++ src/main/java/com/example/tendermegadev/controller/MainItemController.java
@@ -1,7 +1,31 @@
 package com.example.tendermegadev.controller;
 
+import com.example.tendermegadev.payload.PayloadUtil;
+import com.example.tendermegadev.payload.ReturnedResult;
+import com.example.tendermegadev.service.MainItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/mainItem")
 public class MainItemController {
+    @Autowired
+    private MainItemService mainItemService;
+    //get all
+    @GetMapping("all")
+    public ResponseEntity<ReturnedResult> mainItems(){
+    ReturnedResult result = null;
+    try {
+        result = PayloadUtil.fillResult(HttpStatus.OK,true,mainItemService.getAllMainItems());
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }catch (Exception e) {
+        result = PayloadUtil.fillResult(HttpStatus.INTERNAL_SERVER_ERROR,
+                false, e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
+    }
+    }
 }
--- src/main/java/com/example/tendermegadev/service/MainItemService.java
+++ src/main/java/com/example/tendermegadev/service/MainItemService.java
@@ -1,7 +1,21 @@
 package com.example.tendermegadev.service;
 
+import com.example.tendermegadev.model.MainItem;
+import com.example.tendermegadev.repository.MainItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MainItemService {
+    @Autowired
+    private MainItemRepository mainItemRepository;
+
+    //get all main items
+    public List<MainItem> getAllMainItems(){
+        return mainItemRepository.findAll();
+    }
+
+
 }
